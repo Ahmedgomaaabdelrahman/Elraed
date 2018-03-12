68bea6f0ce9d1d129fac630c4390d43c2ed03bd3
Gomaa <a.gomaaabdelrahman@gmail.com>
2018-03-12 16:53:43 +0200

    kl

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -29,7 +29,8 @@ export class MyApp {
       // Here you can do any higher level native things you might need.
       statusBar.styleDefault();
       splashScreen.hide();
-this.fcm.pushPlugin();
+
+
 
       this.fcm.onTokenRecived(this.nav).then((res)=>{
         if(res){
